08fe2d15eb793f2d913399a9e462ee597a254dd8
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2017-10-29 16:56:42 +0000

    Improve styling on generic links

--- src/components/app-body.vue
+++ src/components/app-body.vue
@@ -2,8 +2,19 @@
     <router-view></router-view>
 </template>
 
-<script>
+<style>
+a, a:visited {
+    color: #20AFDB;
+    text-decoration: none;
+}
 
+a:hover {
+    color: #57F6FF;
+}
+</style>
+
+<script>
 export default {
+
 };
 </script>
