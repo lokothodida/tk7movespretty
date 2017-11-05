318b80ec8fcd1d81548245f7b51294fd1b01ea30
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2017-11-05 15:09:41 +0000

    Move hit level styles into hit level component

--- src/components/app.vue
+++ src/components/app.vue
@@ -556,33 +556,6 @@ div.move-hitlvlstring::-webkit-scrollbar {
     height: 3px !important;
 }
 
-.mv-hitlvl {
-    margin: 0;
-    text-transform: uppercase;
-    font-weight: bold;
-    text-shadow: 0px 0px 5px #000000;
-}
-
-.hithigh {
-    color:  #ffc6ff;
-}
-
-.hitmid {
-    color: #ffff79;
-}
-
-.hitlow {
-    color:  #9cd3fc;
-}
-
-.hitsmid {
-    color: #00d56d;
-}
-
-.hitnoblk {
-    color: #0cc;
-}
-
 .fa-chevron-right, .fa-caret-right {
     margin: 0 5px 0 5px;
 }
--- src/components/move-hit-level.vue
+++ src/components/move-hit-level.vue
@@ -4,6 +4,35 @@
     </p>
 </template>
 
+<style lang="scss">
+.mv-hitlvl {
+    margin: 0;
+    text-transform: uppercase;
+    font-weight: bold;
+    text-shadow: 0px 0px 5px #000000;
+}
+
+.hithigh {
+    color:  #ffc6ff;
+}
+
+.hitmid {
+    color: #ffff79;
+}
+
+.hitlow {
+    color:  #9cd3fc;
+}
+
+.hitsmid {
+    color: #00d56d;
+}
+
+.hitnoblk {
+    color: #0cc;
+}
+</style>
+
 <script>
 export default {
     props: ['hit', 'index'],
