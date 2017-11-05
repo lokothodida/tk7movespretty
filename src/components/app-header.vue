0743a02f87e29de9221c9c14c3af3c3fd043638c
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2017-11-05 15:00:17 +0000

    Move header scss to app-header component

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -5,6 +5,54 @@
     </div>
 </template>
 
+<style lang="scss">
+.header {
+    display: flex;
+    align-content: center;
+    align-items: center;
+    padding: 15px;
+    flex-direction: row;
+    max-width: 100%;
+    max-height: 100px;
+    top: 0;
+    left: 0;
+    justify-content: flex-start;
+
+    > img {
+        margin: 0;
+        max-width: 300px;
+        min-width: 150px;
+        height: auto;
+        flex-grow: 1.2;
+        flex-basis: 0;
+    }
+
+    > h3 {
+        margin: 0;
+        color: #ffffff;
+        font-family: "Trump", Arial, sans-serif;
+        font-size: 50px;
+        margin-top: 5px;
+        font-style: italic;
+        margin-left: -15px;
+        align-self: flex-start;
+        flex-grow: 0.8;
+        flex-basis: 0;
+    }
+}
+
+@media only screen and (max-width: 801px) {
+    .header {
+        padding: 0;
+    }
+
+    .header > h3 {
+        font-size: 45px;
+        margin-top: 8px;
+    }
+}
+</style>
+
 <script>
 export default {
     data() {
--- src/components/app.vue
+++ src/components/app.vue
@@ -54,41 +54,6 @@
   user-select: none;
 }
 
-.header {
-    display: flex;
-    align-content: center;
-    align-items: center;
-    padding: 15px;
-    flex-direction: row;
-    max-width: 100%;
-    max-height: 100px;
-    top: 0;
-    left: 0;
-    justify-content: flex-start;
-
-    > img {
-        margin: 0;
-        max-width: 300px;
-        min-width: 150px;
-        height: auto;
-        flex-grow: 1.2;
-        flex-basis: 0;
-    }
-
-    > h3 {
-        margin: 0;
-        color: #ffffff;
-        font-family: "Trump", Arial, sans-serif;
-        font-size: 50px;
-        margin-top: 5px;
-        font-style: italic;
-        margin-left: -15px;
-        align-self: flex-start;
-        flex-grow: 0.8;
-        flex-basis: 0;
-    }
-}
-
 .glow {
     -webkit-animation-duration: 1s;
     -webkit-animation-name: glow;
@@ -918,15 +883,6 @@ p.track {
 }
 
 @media only screen and (max-width: 801px) {
-    .header {
-        padding: 0;
-    }
-
-    .header > h3 {
-        font-size: 45px;
-        margin-top: 8px;
-    }
-
     .move-head {
         font-size: 35px;
     }
