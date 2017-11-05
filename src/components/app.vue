e11cc4f7dbc605790d4bdccd867ca290266b83a1
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2017-11-05 15:47:30 +0000

    Move character list styles to component

--- src/components/app.vue
+++ src/components/app.vue
@@ -107,84 +107,6 @@ table {
     overflow: hidden;*/
 }
 
-.char-menu {
-    width: 200px;
-    height: 100%;
-    margin-left: 5px;
-
-    @media only screen and (max-width: 800px){
-        position: absolute;
-        background-color: rgba(0,0,0,0.85);
-        z-index: 10;
-        margin-left: 3px;
-        width: 155px;
-        border: 1px solid rgba(12,73,90,.75);
-        height: 100%;
-        border-radius: 5px;
-        display: none;
-    }
-}
-
-.char-menu ::-webkit-scrollbar {
-    width: 8px;
-    @media only screen and (max-width: 800px){
-        width: 5px;
-    }
-}
-
-.char-menu .head {
-    width: 100%;
-}
-
-.char-menu .inner-table {
-    @media only screen and (max-width: 800px){
-        height: calc(100% - 40px);
-    }
-}
-
-.char-menu-open {
-    width: 30px;
-    font-size: 20px;
-    color: #ffffff;
-    text-shadow: 0 0 6px #0af1f5;
-    text-align: center;
-    background-color: rgba(0, 0, 0, 0.75);
-    border-radius: 44px;
-    padding: 3px 0px 3px 0px;
-    border: 1px solid rgba(12,73,90,.75);
-    display: none;
-    cursor: pointer;
-
-    @media only screen and (max-width: 800px){
-        display: initial;
-    }
-
-}
-
-.char-menu-open:active {
-    background-color: rgba(7,44,54,.75);
-}
-
-.char-menu-close {
-    font-size: 25px;
-    width: 25px;
-    color: #ffffff;
-    vertical-align: middle;
-    text-align: right;
-    padding: 3px 2px 2px 2px;
-    text-shadow: 0 0 10px #fd0106;
-    display: none;
-    cursor: pointer;
-
-    @media only screen and (max-width: 800px){
-        display: table-cell;
-    }
-}
-
-.char-menu-close:active {
-    text-shadow: 0 0 10px #fd0106;
-}
-
 .char-movelist {
     /*width: 600px;*/
     max-width: 970px;
@@ -289,94 +211,6 @@ table {
     }
 }
 
-.char-head {
-    color: #ffffff;
-    font-family: "Trump", Arial, sans-serif;
-    font-size: 40px;
-    font-style: italic;
-    text-shadow: 0 0 5px hsla(181, 92%, 50%, 1);
-    text-transform: uppercase;
-    text-align: center;
-
-    @media only screen and (max-width: 800px) {
-        font-size: 25px;
-        vertical-align: middle;
-        text-align: left;
-        padding-left: 5px;
-    }
-}
-
-.char-card {
-    overflow: hidden;
-    position: relative;
-    display: block;
-    height: 100px;
-    border: 2px solid hsla(193, 77%, 20%, 0.75);
-    background-color: rgba(0,0,0,0.50);
-    border-radius: 3px;
-
-    @media only screen and (max-width: 800px) {
-        border-width: 1px;
-        height: 75px;
-        width: 140px;
-    }
-}
-
-.char-card:hover {
-    border: 2px solid hsla(193, 77%, 40%, 0.75);
-    /*box-shadow: inset 0 0 14px 4px hsla(193, 77%, 40%, 0.75);*/
-    outline: none;
-    cursor: pointer;
-
-    -webkit-animation-duration: 0.5s;
-    -webkit-animation-name: char-card-glow;
-    -webkit-animation-direction: alternate;
-    -webkit-animation-iteration-count: infinite;
-    animation-duration: 0.5s;
-    animation-name: char-card-glow;
-    animation-direction: alternate;
-    animation-iteration-count: infinite;
-
-}
-
-@-webkit-keyframes char-card-glow {
-    from { box-shadow: inset 0 0 100px 0px hsla(193, 77%, 40%, 0.75);  }
-    to { box-shadow: inset 0 0 150px 10px hsla(193, 77%, 40%, 0.75); }
-}
-
-
-.selected {
-    border: 2px solid hsla(193, 77%, 40%, 0.75);
-    box-shadow: inset 0 0 14px 4px hsla(193, 77%, 40%, 0.75);
-}
-
-.char-card img {
-    width: 100%;
-    height: auto;
-    position: absolute;
-    bottom: 0px;
-    right: 0px;
-}
-
-.char-card p {
-    color: #ffffff;
-    font-family: "Core Sans G", Arial, sans-serif;
-    font-style: italic;
-    background-color: rgba(10,10,10,0.50);
-    font-size: 25px;
-    text-transform: uppercase;
-    text-align: left;
-    padding-left: 5px;
-    width: 100%;
-    bottom: 0px;
-    margin: 0 0 0 -1px;
-    position: absolute;
-
-    @media only screen and (max-width: 800px) {
-        font-size: 15px;
-    }
-}
-
 .move-table {
     /*background-color: rgba(0,0,0,0.25);*/
     border: 1px solid hsla(193, 77%, 40%, 0.25);
@@ -385,10 +219,6 @@ table {
     max-width: 100%;
     //width: auto;
 }
-
-/* Move Cards CSS */
-
-
 
 .move-hit-dmg * {
     @media only screen and (max-width: 800px) {
@@ -434,9 +264,6 @@ table {
 .hidden {
     display: none;
 }
-
-
-
 
 .fa-chevron-right, .fa-caret-right {
     margin: 0 5px 0 5px;
